f05842bee41dfbd8b1941afec05096cbe4418616
robertdfrench <frenchrd@ornl.gov>
2017-06-15 16:04:00 -0400

    Mock Githost Client

--- ghal/__init__.py
+++ ghal/__init__.py
@@ -1,2 +1,10 @@
 def hello():
     return True
+
+
+class GithostConnection(object):
+    healthy = True
+
+
+def connect(uri, key):
+    return GithostConnection()
--- tests/test_ghal.py
+++ tests/test_ghal.py
@@ -1,5 +1,19 @@
 import ghal
+import pytest
+import json
+
+
+@pytest.fixture(scope='module')
+def config():
+    with open("config.json") as config_file:
+        return json.loads(config_file.read())
 
 
 def test_ghal_says_hello():
     assert ghal.hello()
+
+
+def test_connect_to_github(config):
+    uri = "%s@github.com" % config['GITHUB_USERNAME']
+    github = ghal.connect(uri, config['GITHUB_API_KEY'])
+    assert github.healthy
