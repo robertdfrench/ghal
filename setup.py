07eed1ad6470b14988a5954f7796a5595a21a895
robertdfrench <frenchrd@ornl.gov>
2017-08-01 12:48:10 -0400

    Drop python27

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
         name='Ghal',
         version='0.1.0',
-        description='Githost Abstraction Layer. One library for Gitlab, Github, and others',
+        description='Githost Abstraction Layer. One library to rule them all',
         url='https://github.com/robertdfrench/ghal',
         packages=['ghal']
         )
